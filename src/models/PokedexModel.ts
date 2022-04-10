490c31097a2b3f6794a4ec85a37cebb807d78ce4
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-04-10 17:26:54 -0300

    feat(pokedexmodel.ts): add pokedexModel and remove old files i use for tests

---
+++ src/models/PokedexModel.ts
@@ -0,0 +1,63 @@
+import { Schema, model as createModel, Document } from 'mongoose';
+import { Pokedex } from '../schemas';
+import MongoModel from './MongoModel';
+
+interface CopaDocument extends Pokedex, Document {}
+
+const pokeSchema = new Schema<CopaDocument>({
+  nome: {
+    type: String,
+    required: true,
+  },
+  tipo: {
+    type: String,
+    required: true,
+  },
+  hp: {
+    type: Number,
+    required: true,
+  },
+  ataque: {
+    type: Number,
+    required: true,
+  },
+  defesa: {
+    type: Number,
+    required: true,
+  },
+  velocidade: {
+    type: Number,
+    required: true,
+  },
+  total: {
+    type: Number,
+    required: true,
+  },
+  habilidades: {
+    type: [{
+      habilidade1: {
+        type: String,
+        required: true,
+      },
+      habilidade2: {
+        type: String,
+        required: true,
+      },
+    }],
+    required: true,
+  },
+  img: {
+    type: String,
+    required: true,
+  },
+});
+
+class PokedexModel extends MongoModel<Pokedex> {
+  constructor(
+    public model = createModel('pokedex', pokeSchema),
+  ) {
+    super(model);
+  }
+}
+
+export default PokedexModel;
