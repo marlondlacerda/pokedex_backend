8b6639eff5c1f339a3e643ff65b941082ab4cb64
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-04-12 22:24:01 -0300

    feat: creating Interface for pokedex

---
+++ src/interfaces/IPokedex.ts
@@ -0,0 +1,30 @@
+export interface IPokedex {
+  id: number,
+  name: string,
+  type: [string],
+  weight: {
+    value: number,
+    measurement: string,
+  },
+  height: {
+    value: number,
+    measurement: string,
+  },
+  description: string,
+  baseStats: {
+    hp: number,
+    atk: number,
+    def: number,
+    satk: number,
+    sdef: number,
+    spd: number
+  },    
+  moves: {
+    skill1: string,
+    skill2: string,
+  }
+  image1: string,
+  image2: string,
+}
+
+export default IPokedex;
