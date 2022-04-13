6769636b9782ebc5c78e70bb7c5c8c9950265e1d
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-04-13 05:34:38 -0300

    feat: creating loginServicer and Login DONE

--- src/services/LoginService.ts
+++ src/services/LoginService.ts
@@ -1,18 +1,37 @@
+import bcrypt from 'bcryptjs';
+
+import createError from '../utils';
 import { LoginModel } from '../models/LoginModel';
 import { UserLogin } from '../schemas';
 import Service from './Service';
+import { Authenticator } from '../middlewares';
 
 class LoginService extends Service<UserLogin> {
   constructor(
-    public model = new LoginModel(),
+    protected model = new LoginModel(),
+    protected authenticate = new Authenticator(),
   ) {
     super(model);
   }
 
-  public read = async (): Promise<UserLogin> => {
-    const result = await this.model.read();
+  readonly login = async (body: UserLogin): Promise<string> => {
+    const { email, password } = body;
 
-    return result as unknown as UserLogin;
+    const result = await this.model.readOne(email);
+
+    if (!result) {
+      throw createError('unauthorized', 'Incorrect email or password');
+    }
+
+    if (!await bcrypt.compare(password, result.password)) {
+      throw createError('unauthorized', 'Incorrect email or password');
+    }
+
+    const { username } = result;
+
+    const token = this.authenticate.generateToken({ username });
+
+    return token;
   };
 }
 
