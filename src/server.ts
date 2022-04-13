08509b20e8c4b00348795122791fa0a54c142cbc
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-04-13 03:25:30 -0300

    feat: installing LoginFactory on server

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,17 @@ import App from './app';
 import { HandlerError, ZodHandlerError } from './middlewares';
 
 import RouterFactory from './Factories/pokedexFactory';
+import { LoginFactory } from './Factories';
 
 const server = new App();
 
 const handleError = new HandlerError();
 const zodHandlerError = new ZodHandlerError();
 
+const loginFactory = LoginFactory.creatLoginRouter();
 const pokedexFactory = RouterFactory.createPokedexRouter();
 
+server.addRouter(loginFactory);
 server.addRouter(pokedexFactory);
 server.errorRouter(handleError, zodHandlerError);
 
