4ed694ea3c26313a6160fe7c062e4a103044d3eb
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-04-15 20:02:32 -0300

    test: creating loginModel tests

---
+++ src/tests/unit/models/loginModel.test.ts
@@ -0,0 +1,36 @@
+import { expect } from "chai"
+import Sinon from "sinon"
+
+import { LoginModel } from "../../../models"
+
+describe('1) - Login Model', () => {
+  let loginModel = new LoginModel()
+
+  describe('1) - Testing readOne LoginModel', () => {
+    const user = {
+      email: "email.test@.com",
+      password: "123456",
+    }
+
+    before(() => {
+      Sinon.stub(loginModel.model, 'findOne').resolves(user);
+    })
+
+    after(() => {
+      Sinon.restore();
+    });
+
+    it('1) - Assert your return is an Array, and length is the same as mock', async () => {
+      const user = {
+        email: "email.test@.com",
+        password: "123456",
+      }
+
+      const result = await loginModel.readOne(user.email)
+      console.log(result);
+      
+      expect(result).to.deep.equal(user);
+    })
+  })
+})
+
