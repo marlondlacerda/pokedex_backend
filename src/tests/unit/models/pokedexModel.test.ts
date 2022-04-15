c29a6cc73c3bdcd06f4128825169f4435b242d9e
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-04-15 00:18:20 -0300

    test: fix the pokedexModel file

--- src/tests/unit/models/pokedexModel.test.ts
+++ src/tests/unit/models/pokedexModel.test.ts
@@ -1,42 +1,59 @@
 import { expect } from "chai"
 import Sinon from "sinon"
-import PokedexModel from "../../../models"
-import PokeApi from "../mocks"
 
+import PokeApi from "../mocks"
+import { PokedexModel } from "../../../models"
+import pokeApi from "../mocks"
 
 describe('Pokedex Model', () => {
   let pokedexModel = new PokedexModel()
 
   describe('Testing getAll PokedexModel', () => {
     before(() => {
-      Sinon.stub(pokedexModel.model, 'find').resolves(PokeApi)
+      Sinon.stub(pokedexModel.model, 'find').resolves(pokeApi)
     })
 
     after(() => {
-      Sinon.restore()
+      Sinon.restore();
     });
 
-    it('Returns all Pokemons with your status and type', async () => {
+    it('Assert your return is an Array, and length is the same as mock', async () => {
       const result = await pokedexModel.read()
 
       expect(result).to.be.an('array')
       expect(result).to.have.lengthOf(PokeApi.length)
-
-      expect(result[0]).to.have.property('nome')
-      expect(result[0]).to.have.property('hp')
-      expect(result[0]).to.have.property('ataque')
-      expect(result[0]).to.have.property('defesa')
-      expect(result[0]).to.have.property('velocidade')
-      expect(result[0]).to.have.property('total')
-      expect(result[0]).to.have.property('habilidades')
-      expect(result[0].habilidades).to.be.an('array')
-
-      expect(result[0].habilidades).to.have.lengthOf(1)
-      expect(result[0].habilidades[0]).to.have.property('habilidade1')
-      expect(result[0].habilidades[0]).to.have.property('habilidade2')
-      expect(result[0]).to.have.property('img')
-
       expect(result).to.deep.equal(PokeApi)
+    })
+
+    it('Check object properties', async () => {
+      const result = await pokedexModel.read()
+
+      expect(result[0]).to.have.property('_id')
+      expect(result[0]).to.have.property('name')
+      expect(result[0]).to.have.property('type')
+
+      expect(result[0]).to.have.property('height')
+      expect(result[0].height).to.have.property('value')
+      expect(result[0].height).to.have.property('measurement')
+
+      expect(result[0]).to.have.property('weight')
+      expect(result[0].weight).to.have.property('value')
+      expect(result[0].weight).to.have.property('measurement')
+
+      expect(result[0]).to.have.property('baseStats')
+      expect(result[0].baseStats).to.have.property('hp')
+      expect(result[0].baseStats).to.have.property('atk')
+      expect(result[0].baseStats).to.have.property('def')
+      expect(result[0].baseStats).to.have.property('satk')
+      expect(result[0].baseStats).to.have.property('sdef')
+      expect(result[0].baseStats).to.have.property('spd')
+
+      expect(result[0]).to.have.property('moves')
+      expect(result[0].moves).to.have.property('skill1')
+      expect(result[0].moves).to.have.property('skill2')
+
+      expect(result[0]).to.have.property('image1')
+      expect(result[0]).to.have.property('image2')
     })
   })
 })
