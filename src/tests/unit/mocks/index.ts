0bdfe84549ce2e42c9606a1596def070c8440089
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-04-15 15:35:48 -0300

    test: functions for mock tests

--- src/tests/unit/mocks/index.ts
+++ src/tests/unit/mocks/index.ts
@@ -1,3 +1,18 @@
-import pokeApi from "./pokeApi"
+import pokeApi from "./pokeApi.json"
+
+const mockCreate = (Instance: typeof pokeApi, data: any) => {
+  if (!data) {
+    return;
+  }
+
+  const newData = data;
+
+  Instance.push(data);
+  return newData;
+};
+
+export const Pokemon = {
+  create: async (data: any) => mockCreate(pokeApi, data),
+};
 
 export default pokeApi;
