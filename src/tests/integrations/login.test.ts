8e7bce7f09384ca3b5d7060ae7254ff35fc2f654
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-04-16 20:21:08 -0300

    test: update and refactor integration test of login

--- src/tests/integrations/login.test.ts
+++ src/tests/integrations/login.test.ts
@@ -2,7 +2,6 @@ const chai = require('chai')
 const chaiHttp = require('chai-http')
 
 import { Response } from 'superagent';
-import { userInput } from '../unit/inputs';
 
 import { invalidUserEmail, invalidUserPassword, user } from './inputs';
 
@@ -22,46 +21,45 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('1) - Endpoint /login', () => {
-  const URL = 'http://localhost:3001';
-
+describe('Integration Test - Endpoint "/login"', () => {
   describe('1) - When Login sucess', () => {
     it('1) - Shoud return status 200 and a token', async () => {
-      const response = await chai
-        .request(app.app)
-        .post(`/login`)
-        .send(user);
-
+      const response: Response = await chai
+      .request(app.app)
+      .post(`/login`)
+      .send(user);
+      
       expect(response.status).to.be.equal(200);
       expect(response.body).to.have.property('token');
-      });
-    })
+    });
+  })
 
   describe('2) - When Login with invalid email or password', () => {
     it('1) - Shoud return error when email is invalid', async () => {
-        const response = await chai
-          .request(app.app)
-          .post('/login')
-          .send(invalidUserEmail);
+      const response: Response = await chai
+      .request(app.app)
+        .post('/login')
+        .send(invalidUserEmail);
 
-        const { status, body } = response;
-  
-        expect(status).to.equal(401);
-        expect(body).to.have.property('error');
-        expect(body.error).to.equal('Incorrect email or password');
+      const { status, body } = response;
+
+      expect(status).to.equal(401);
+      expect(body).to.have.property('error');
+      expect(body.error).to.equal('Incorrect email or password');
     })
-
+    
     it('2) - Shoud return error when password is invalid', async () => {
-        const response = await chai
-          .request(app.app)
-          .post('/login')
-          .send(invalidUserPassword);
-  
-        const { status, body } = response;
-  
-        expect(status).to.equal(401);
-        expect(body).to.have.property('error');
-        expect(body.error).to.equal('Incorrect email or password');
+      const response: Response = await chai
+      .request(app.app)
+      .post('/login')
+        .send(invalidUserPassword);
+        
+      const { status, body } = response;
+      
+      expect(status).to.equal(401);
+      expect(body).to.have.property('error');
+      expect(body.error).to.equal('Incorrect email or password');
     })
   })
 })
+
