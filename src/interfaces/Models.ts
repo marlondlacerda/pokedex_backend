d8c10b26e4caf58abcc313369c0f531a7ff5c784
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-04-17 19:23:39 -0300

    feat: creating Update for MongoModel

--- src/interfaces/Models.ts
+++ src/interfaces/Models.ts
@@ -1,6 +1,7 @@
 interface Model<T> {
   create(obj: T): Promise<T>,
   read(): Promise<T | T[]>;
+  update(_id: number, obj: T): Promise<T | null>,
 }
 
 export default Model;
--- src/models/MongoModel.ts
+++ src/models/MongoModel.ts
@@ -9,6 +9,9 @@ abstract class MongoModel<T> implements Model<T> {
   create = async (obj: T): Promise<T> => this.model.create({ ...obj });
 
   read = async (): Promise<T[]> => this.model.find({}, {}, { lean: true });
+
+  update = async (_id: number, obj: T): Promise<T | null> =>
+    this.model.findOneAndUpdate({ _id }, { obj });
 }
 
 export default MongoModel;
