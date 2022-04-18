bd2231bd9e8677967bfc44c5705b0a15c587ca85
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-04-18 19:44:11 -0300

    test: create test for findONe

--- src/tests/unit/models/pokedexModel.test.ts
+++ src/tests/unit/models/pokedexModel.test.ts
@@ -8,7 +8,34 @@ import { pokemonInput, pokemonPartialUpdateInput, pokemonUpdateInput } from "../
 describe('Unit Test - Pokedex Model', () => {
   const pokedexModel = new PokedexModel()
 
-  describe('1) - Testing getAll PokedexModel', () => {
+  describe('1) - Testing Create of PokedexModel', () => {
+    before(() => {
+      Sinon.stub(pokedexModel.model, 'create').callsFake(Pokemon.create)
+    })
+
+    after(() => {
+      Sinon.restore();
+      pokeApi.pop()
+    });
+
+    describe('1) - Before being tested', () => {
+      it('1) - Should return 2 records', async () => {
+        expect(pokeApi.length).to.be.equal(2)
+      })
+    })
+
+    describe('2) - After being tested', () => {
+      it('1) - Should return 3 records', async () => {
+        const result = await pokedexModel.create(pokemonInput)
+
+        expect(pokeApi.length).to.be.equal(3)
+        expect(result).to.be.an('object')
+        expect(result).to.deep.equal(pokeApi[2])
+      })
+    })
+  })
+
+  describe('2.1) - Testing getAll PokedexModel', () => {
     before(() => {
       Sinon.stub(pokedexModel.model, 'find').resolves(pokeApi)
     })
@@ -56,32 +83,54 @@ describe('Unit Test - Pokedex Model', () => {
       expect(result[0]).to.have.property('image2')
     })
   })
-  describe('2) - Testing Create of PokedexModel', () => {
+
+  describe('2.2 - Test getById PokedexModel', () => {
     before(() => {
-      Sinon.stub(pokedexModel.model, 'create').callsFake(Pokemon.create)
+      Sinon.stub(pokedexModel.model, 'findById').resolves(pokeApi[0])
     })
 
     after(() => {
       Sinon.restore();
-      pokeApi.pop()
     });
 
-    describe('1) - Before being tested', () => {
-      it('1) - Should return 2 records', async () => {
-        expect(pokeApi.length).to.be.equal(2)
-      })
+    it('1) - Assert your return is an Object', async () => {
+      const result = await pokedexModel.readOne(1)
+
+      expect(result).to.be.an('object')
+      expect(result).to.deep.equal(pokeApi[0])
     })
 
-    describe('2) - After being tested', () => {
-      it('1) - Should return 3 records', async () => {
-        const result = await pokedexModel.create(pokemonInput)
+    it('2) - Check object properties', async () => {
+      const result = await pokedexModel.readOne(1)
 
-        expect(pokeApi.length).to.be.equal(3)
-        expect(result).to.be.an('object')
-        expect(result).to.deep.equal(pokeApi[2])
-      })
+      expect(result).to.have.property('_id')
+      expect(result).to.have.property('name')
+      expect(result).to.have.property('type')
+
+      expect(result).to.have.property('height')
+      expect(result.height).to.have.property('value')
+      expect(result.height).to.have.property('measurement')
+
+      expect(result).to.have.property('weight')
+      expect(result.weight).to.have.property('value')
+      expect(result.weight).to.have.property('measurement')
+
+      expect(result).to.have.property('baseStats')
+      expect(result.baseStats).to.have.property('hp')
+      expect(result.baseStats).to.have.property('atk')
+      expect(result.baseStats).to.have.property('def')
+      expect(result.baseStats).to.have.property('satk')
+      expect(result.baseStats).to.have.property('sdef')
+      expect(result.baseStats).to.have.property('spd')
+
+      expect(result).to.have.property('moves')
+      expect(result.moves).to.have.property('skill1')
+      expect(result.moves).to.have.property('skill2')
+
+      expect(result).to.have.property('image1')
+      expect(result).to.have.property('image2')
     })
-  })
+  });
 
   describe('3.1) - Testing Update of PokedexModel', () => {
     before(() => {
