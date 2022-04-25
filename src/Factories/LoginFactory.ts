f5bda0cdc32186d9cd0f49cf9f98b3df16d64259
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-04-25 17:51:52 -0300

    feat: 🎸 update LoginFactory

--- src/Factories/LoginFactory.ts
+++ src/Factories/LoginFactory.ts
@@ -1,20 +1,19 @@
 import { Router } from 'express';
-import { LoginValidation } from '../validations';
+import { Authenticator, Validation } from '../middlewares';
+import { LoginModel } from '../models';
+import { LoginService } from '../services';
 import { LoginController } from '../controllers';
 import { LoginRouter } from '../routes';
-import { Authenticator } from '../middlewares';
-import { LoginService } from '../services';
-import { LoginModel } from '../models';
 
 class LoginFactory {
   public static createLoginRouter(): Router {
-    const loginValidation = new LoginValidation();
+    const validation = new Validation();
     const authenticator = new Authenticator();
     const loginModel = new LoginModel();
     const loginService = new LoginService(loginModel, authenticator);
     const loginController = new LoginController(loginService);
     const loginRouter = new LoginRouter();
-    loginRouter.addRoute(loginController, loginValidation);
+    loginRouter.addRoute(loginController, validation);
     return loginRouter.router;
   }
 }
